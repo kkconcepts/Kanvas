12efb6359143e971c6d2fe7af30ab28319402ee3
kkconcepts <powell@kevinkelleyconcepts.com>
2024-08-06 16:07:48 -0500

    updates to 404

--- web/app.vue
+++ web/app.vue
@@ -1,29 +1,15 @@
 <script setup>
 import '@unocss/reset/tailwind.css'
-
-const isLoading = ref(true);
-
-onMounted(() => {
-  // Simulate loading data
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 2000);
-});
 </script>
 
 <template>
     <div class="__layout">
         <div class="page">
             <Header />
-            <div v-if="isLoading">
-                <Loader />
-            </div>
-            <div v-else>
-            </div>
-                <main class="main">
-                    <NuxtPage />
-                </main>
-                <BottomFooter />
+              <main class="main">
+                  <NuxtPage />
+              </main>
+              <BottomFooter />
         </div>
     </div>
 </template>
--- web/error.vue
+++ web/error.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex h-screen w-full items-center justify-center flex-col">
-    <h2>Page Not Found</h2>
-    <button class="underline pt-8" @click="handleError">Back Home</button>
+  <div class="bg-black flex h-screen w-full items-center justify-center flex-col f-gap-16-24">
+    <h1 class="f-text-16-24">Page Not Found</h1>
+    <button class="reservation-btn hover:opacity-80 transition-65 cursor-pointer z-4 b-rounded-1.5 f-px-10-24 f-pt-8-12 f-pb-8-12 f-text-13-15" @click="handleError">Back Home</button>
   </div>
 </template>
 
